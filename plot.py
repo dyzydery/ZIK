0deea715240ef96ad4aac18c2e040f985cc8288d
dyzydery <maciekdudek@gmail.com>
2020-10-09 16:43:33 +0200

    plot z podpisem

--- plot.py
+++ plot.py
@@ -12,18 +12,35 @@ q1 = "SELECT timestamp,kielecki FROM 'ceny';"
 q2 = "SELECT timestamp,kielecki FROM 'zik';"
 
 df1 = pd.read_sql_query(q1,conn)
-df2 = pd.read_sql_query(q1,conn)
+df2 = pd.read_sql_query(q2,conn)
 
 # Create a visualization
 ax = sns.lineplot(
-    data=df1,
+    data=df2,color="red",
     x="timestamp", y="kielecki", )
 ax.set(xlabel='Czas', ylabel='PLN', title='Ceny Kieleckiego')
-ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
-# plt.show()
+# ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
+ax.set(xticks=df2.timestamp[2::50])
+ax2 = ax.twinx()
+ax = sns.lineplot(
+    data=df1,
+    x="timestamp", y="kielecki", )
 
-# ax = df1.plot(x="timestamp", y="kielecki", legend=False)
-# ax2 = ax.twinx()
-# df2.plot(x="timestamp", y="kielecki", ax=ax2, legend=False, color="r")
-# ax.figure.legend()
+ts = df1["timestamp"]
+# xticks and xticks labels
+xTicks = list(range(len(ts)))
+
+xTickLabels = [i for i in range(len(ts))]
+for xTick in xTicks:
+    if xTick % 50 == 0:
+        xTickLabels[xTick] = ts[xTick][:10]
+    else:
+        xTickLabels[xTick] = ''
+        xTick = 0
+
+
+ax.xaxis.grid(True)
+ax.set_xticks(xTicks)
+ax.set_xticklabels(xTickLabels, fontsize=6)
+
 plt.show()
