3b997b8456ad00cac9011b9136026d58ecc6d0e1
dyzydery <maciekdudek@gmail.com>
2020-10-12 12:42:13 +0200

    pierwszy batch wykresow

--- plot.py
+++ plot.py
@@ -2,45 +2,48 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-tips = pd.read_csv('zikDB.csv')
-# Apply the default theme
-sns.set_theme()
+
 import sqlite3
 
-conn = sqlite3.connect('zik.db')
-q1 = "SELECT timestamp,kielecki FROM 'ceny';"
-q2 = "SELECT timestamp,kielecki FROM 'zik';"
+def savePlot(produkt):
+    sns.set_theme()
+    conn = sqlite3.connect('zik.db')
+    q1 = "SELECT timestamp,"+produkt+" FROM 'ceny';"
+    q2 = "SELECT timestamp,"+produkt+" FROM 'zik';"
 
-df1 = pd.read_sql_query(q1,conn)
-df2 = pd.read_sql_query(q2,conn)
+    df1 = pd.read_sql_query(q1,conn)
+    df2 = pd.read_sql_query(q2,conn)
 
-# Create a visualization
-ax = sns.lineplot(
-    data=df2,color="red",
-    x="timestamp", y="kielecki", )
-ax.set(xlabel='Czas', ylabel='PLN', title='Ceny Kieleckiego')
-# ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
-ax.set(xticks=df2.timestamp[2::50])
-ax2 = ax.twinx()
-ax = sns.lineplot(
-    data=df1,
-    x="timestamp", y="kielecki", )
+    # Create a visualization
+    ax = sns.lineplot(
+        data=df2,color="red",
+        x="timestamp", y=produkt, )
+    ax.set(xlabel='Czas', ylabel='PLN', title=produkt)
+    # ax.set(xticks=df2.timestamp[2::50])
+    ax2 = ax.twinx()
+    ax = sns.lineplot(
+        data=df1,
+        x="timestamp", y=produkt, )
 
-ts = df1["timestamp"]
-# xticks and xticks labels
-xTicks = list(range(len(ts)))
+    ts = df1["timestamp"]
+    xTicks = list(range(len(ts)))
 
-xTickLabels = [i for i in range(len(ts))]
-for xTick in xTicks:
-    if xTick % 50 == 0:
-        xTickLabels[xTick] = ts[xTick][:10]
-    else:
-        xTickLabels[xTick] = ''
-        xTick = 0
+    xTickLabels = [i for i in range(len(ts))]
+    for xTick in xTicks:
+        if xTick % 50 == 0:
+            xTickLabels[xTick] = ts[xTick][:10]
+        else:
+            xTickLabels[xTick] = ''
+            xTick = 0
 
+    ax.xaxis.grid(True)
+    ax.set_xticks(xTicks)
+    ax.set_xticklabels(xTickLabels, fontsize=6)
+    plt.savefig("wykresy/"+produkt+".png")
+    plt.clf()
 
-ax.xaxis.grid(True)
-ax.set_xticks(xTicks)
-ax.set_xticklabels(xTickLabels, fontsize=6)
+produkty = ['kielecki','pizza','maslo','jablka','makaron','chleb','mydlo','kurczak','jajka','rolex','whisky','piwo','buty','auto_Mean','auto_Median','telefon','bigmac','m2wtorny','m2pierwotny','benzyna','lot','fryzjer','upc','prad','lekarz_Mean','lekarz_Median','aspiryna','karma','kasjer','xau','chf','usd','kindle']
 
-plt.show()
+for p in produkty:
+    savePlot(p)
+# plt.show()
